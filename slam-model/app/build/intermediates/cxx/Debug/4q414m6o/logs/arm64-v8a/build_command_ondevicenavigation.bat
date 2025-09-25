56b564cdf2ef690ff2903dbc125f8f2932888735
Tramanata <tramanata@gmail.com>
2025-09-25 11:27:58 -0400

    initial commit
    added opencv android SDK, and it built

---
+++ slam-model/app/build/intermediates/cxx/Debug/4q414m6o/logs/arm64-v8a/build_command_ondevicenavigation.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\trama\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\trama\\OneDrive\\Documents\\GitHub\\OnDevice-Navigation-Assistant\\slam-model\\app\\.cxx\\Debug\\4q414m6o\\arm64-v8a" ^
+  ondevicenavigation
